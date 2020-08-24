3b4022f1a39bf8d24374993fd99894305eb4a230
pakorek <brice.darmenia@sfr.fr>
2020-08-25 00:44:57 +0200

    create Market Model and migration

--- farmer_market/models.py
+++ farmer_market/models.py
@@ -13,3 +13,22 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Market(models.Model):
+    place = models.CharField(max_length=255)
+    day = models.CharField(max_length=12)
+    _from = models.CharField(max_length=5)
+    _to = models.CharField(max_length=5)
+    long = models.SmallIntegerField()
+    lat = models.SmallIntegerField()
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+
+    # nb_farmers = models.ForeignKey('NbFarmer', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "market"
+        ordering = ['city']
+
+    def __str__(self):
+        return self.place
